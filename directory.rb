5fcc0f64b445f00becf842c0d18c7bc005e207f2
Thomas R. Harper <tommyrharper@gmail.com>
2020-03-10 19:19:05 +0000

    Load students from students.csv added

--- directory.rb
+++ directory.rb
@@ -1,5 +1,14 @@
 @students = [] # an empty array accessible to all methods
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def save_students
   # open the file for writing
   file = File.open("students.csv", "w")
@@ -38,6 +47,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -55,6 +65,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
